59cfe18db9a566b08de7e5ecf2073dc026256050
Agga-07 <143468816+Agga-07@users.noreply.github.com>
2024-10-22 13:47:10 -0300

    Update exercicio3.c

--- exercicio3.c
+++ exercicio3.c
@@ -1,24 +1,20 @@
-// Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
-#include <stdio.h>
-#include <string.h>
+//3) Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
 
-int contarA(char *str) {
-    int count = 0;
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] == 'a' || str[i] == 'A') {
-            count++;
-        }
-    }
-    return count;
-}
+Ao final do processamento, qual será o valor da variável SOMA?
+#include <stdio.h>
 
 int main() {
-    char str[100];
-    printf("Digite uma string: ");
-    fgets(str, sizeof(str), stdin);
-    
-    int quant = contarA(str);
-    printf("A letra 'a' aparece %d vez(es) na string.\n", quant);
+    int INDICE = 12;
+    int SOMA = 0;
+    int K = 1;
 
+    while (K < INDICE) {
+        K = K + 1;  // Incrementa K
+        SOMA = SOMA + K;  // Adiciona K à SOMA
+    }
+
+    printf("O valor de SOMA é: %d\n", SOMA);
     return 0;
 }
+
+//o valor de SOMA será 77
